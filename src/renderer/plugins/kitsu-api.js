a233d85c75ff1c95f4c085fdfca4ab932d3f70ab
Luna <luna@stor.lol>
2017-08-19 19:50:19 +1000

    used correct kitsu package

--- src/renderer/plugins/kitsu-api.js
+++
@@ -1,4 +0,0 @@
-import Vue from 'vue'
-import KitsuAPI from 'kitsu-api'
-
-Vue.prototype.kitsu = new KitsuAPI()
---
+++ src/renderer/plugins/kitsu.js
@@ -0,0 +1,4 @@
+import Vue from 'vue'
+import Kitsu from 'kitsu'
+
+Vue.prototype.$kitsu = new Kitsu()
