cd7ce74ea6a74332f048430168e815bd25f98cec
Luna <luna@stor.lol>
2017-08-19 17:57:25 +1000

    added persistent state support using localstorage

--- src/renderer/store/index.js
+++ src/renderer/store/index.js
@@ -3,9 +3,14 @@ import Vuex from 'vuex'
 
 import modules from './modules'
 
+import createPersistedState from 'vuex-persistedstate'
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules,
+  plugins: [createPersistedState({
+    key: 'cinnamon'
+  })],
   strict: process.env.NODE_ENV !== 'production'
 })
