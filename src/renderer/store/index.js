bc5ef94f7000c436197ada8bce7f144a0a48809c
Luna <luna@stor.lol>
2017-08-21 23:15:47 +1000

    whitelisted preferences for state persistence so whole state isn't persisted

--- src/renderer/store/index.js
+++ src/renderer/store/index.js
@@ -10,7 +10,8 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   modules,
   plugins: [createPersistedState({
-    key: 'cinnamon'
+    key: 'cinnamon',
+    paths: ['preferences']
   })],
   strict: process.env.NODE_ENV !== 'production'
 })
